0f4a0c6e124401a8c431f7485eb8e6818b3fd0fa
Cespresso <2cespresso@gmail.com>
2019-05-10 21:31:54 +0900

    modelを一つに

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -1,14 +1,55 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 import 'package:scoped_model/scoped_model.dart';
+import 'package:todoy_flutter/model/entity/todo.dart';
 
-class TodoModel extends Model{
-  int _counter = 0;
+class TodoModel extends Model {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  int get counter => _counter;
+  FirebaseUser user;
+  List<Todo> todos;
 
-
-  void increment() {
-    _counter++;
-    //状態を変更したらnotifyListenersを呼ぶ。
+  login() async {
+    user = await _handleSignIn();
     notifyListeners();
+  }
+
+  getTodos() async {
+    if (user == null) return;
+    String token = await user.getIdToken();
+    var response = await http.get(
+      "https://espresso-dev-api.site/todo",
+      headers: {"authorization": "Bearer "+token},
+    );
+    print(response.body);
+    if(response.statusCode == 200){
+      List todoList = jsonDecode(response.body);
+      todos = todoList.map((dynamic t)=>Todo.fromJson(t)).toList();
+      print(todos);
+    }else{
+      todos = [];
+    }
+    notifyListeners();
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    final GoogleSignInAccount deleteUserResult =
+        await _googleSignIn.disconnect().catchError((err) {});
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser user = await _auth.signInWithCredential(credential);
+    print("signed in " + user.displayName);
+    return user;
   }
 }
